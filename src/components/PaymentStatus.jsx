b6751f0eeffb3aeb708db8d978db4bc3122aafe1
Dylan White <white.dylan@live.co.uk>
2024-07-08 20:45:13 +0100

    Create payment confirmation page

---
+++ src/components/PaymentStatus.jsx
@@ -0,0 +1,50 @@
+import { useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+
+const PaymentStatus = () => {
+  const stripe = useStripe();
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    // Retrieve the "payment_intent_client_secret" query parameter appended to
+    // your return_url by Stripe.js
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    // Retrieve the PaymentIntent
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage(
+            `Success! Payment received. Confirmation email will be sent to ${paymentIntent.receipt_email}`
+          );
+          break;
+
+        case "processing":
+          setMessage(
+            "Payment processing. We'll update you when payment is received."
+          );
+          break;
+
+        case "requires_payment_method":
+          // Redirect your user back to your payment page to attempt collecting
+          // payment again
+          setMessage("Payment failed. Please try another payment method.");
+          break;
+
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+
+  return message;
+};
+
+export default PaymentStatus;
