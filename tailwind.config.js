55fa906064d5e393ed09f52fde78901f2bb90094
Dylan White <white.dylan@live.co.uk>
2024-07-23 22:02:50 +0100

    Format header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sandal-yellow": "#F6C157",
+      },
+      maxHeight: {
+        700: "700px",
+      },
+      height: {
+        700: "700px",
+        525: "525px",
+      },
+    },
   },
   plugins: [],
 };
