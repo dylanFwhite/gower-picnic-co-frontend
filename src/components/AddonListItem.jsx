650a77de29549d73f7293c3cdf93a3de5208c57f
Dylan White <white.dylan@live.co.uk>
2024-11-02 14:25:16 +0000

    format addon list

--- src/components/AddonListItem.jsx
+++ src/components/AddonListItem.jsx
@@ -13,8 +13,11 @@ const AddonListItem = ({ basket, item }) => {
     setQuantity(e.target.value);
   };
   return (
-    <div className="flex flex-row">
-      <span>
+    <div className="flex flex-row p-2">
+      <span
+        style={{ fontFamily: "Roboto", color: "#5C5C5C" }}
+        className="font-normal"
+      >
         {item.label}:
         {new Intl.NumberFormat("en-UK", {
           style: "currency",
@@ -27,12 +30,12 @@ const AddonListItem = ({ basket, item }) => {
         min={1}
         value={quantity}
         onChange={handleChange}
-        className="border-solid border-2 border-slate-500 w-12 pl-2"
+        className="border-solid border-2 border-slate-500 w-12 p-2 mx-2"
       />
 
       <button
         onClick={() => handleAddBasket(item.value, quantity)}
-        className=" h-8 w-36 bg-sandal-yellow hover:bg-amber-200"
+        className=" h-10 w-24 bg-sandal-yellow hover:bg-amber-200 mx-2"
       >
         <span
           style={{ fontFamily: "Roboto" }}
